ac1e7044842e589f5a7cf74d00571c2def64cef8
Sofiya-Sinitsina <sofsin31@gmail.com>
2026-03-05 20:10:27 +0500

    FlatList implementation

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,27 @@
-import { images } from "@/constants";
-import { Image, Text, View } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-blue-100">
-      <Image source = {images.avatar} className="w-72 h-72 m-4" />
+    <SafeAreaView>
+      {/* <Image source = {images.avatar} className="w-72 h-72 m-4" />
       <Image source = {images.avocado} className="w-64 h-64 m-4" />
-      <Text className="text-3xl text-primary font-quicksand-medium">Edited screen's text.</Text>
-    </View>
+      <Text className="text-3xl text-primary font-quicksand-medium">Edited screen's text.</Text> */}
+      <FlatList
+          data = {offers} 
+          renderItem={
+            ({item, index}) => {
+              return (
+                <View>
+                  <Pressable className="my-3 h-[100px]" style={{backgroundColor: item.color}}>
+                    <Text className="mx-auto">{item.title}</Text>
+                  </Pressable>
+                </View>
+              )
+            }
+          }
+      />
+    </SafeAreaView>
   );
 }
